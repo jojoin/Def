87229b36e2d7aa4b33498ae4e26f9f098ab73c6f
yangjiePro <yangjie@jojoin.com>
2015-06-18 16:09:18 +0800

    up

--- test/item/func.d
+++ test/item/func.d
@@ -8,14 +8,15 @@ p : 123
 
 # 函数定义（默认参数和关键字参数）
 # 
-defun myfunc(a b c:p d:"ddd")
-	nnn : "new insert stark "
-	if a-1
-		return nnn+d
+defun test(a b c:p d:"ddd")
+	nnn : "cond 'a' is true, return ! "
+	if a
+		return nnn
 	;
-	"after if return"
+	"$a is false , d: "+d
 ;
 
 
-
+defun ept()
+;
 
