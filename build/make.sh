ed4d4c8ef2ef4652f8c54ab5e222c6cb211ecf81
yangjiePro <yangjie@jojoin.com>
2015-06-29 15:21:12 +0800

    up

--- build/make.sh
+++ build/make.sh
@@ -7,11 +7,13 @@ echo "- - - - - - - - - - - - - - - - - - - - - - -"
 cd ../src
 
 # 合并文件 exec.cc
-set target="./vm/exec.cc"
+target=./vm/exec.cc
 cat ./exec/_header.c > $target
-for file in ./exec/*.cc do cat $file >> $target done
+for file in ./exec/*.cc
+do 
+	cat $file >> $target 
+done
 cat ./exec/_footer.c >> $target
-
 
 # 开始编译
 g++ ./*.cc ./util/*.cc ./parse/*.cc ./object/*.cc ./vm/*.cc -std=c++11 -w -o ../build/def 
